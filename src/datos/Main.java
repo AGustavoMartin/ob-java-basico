92c9d0b497507281adfdace52c8c1185ce240e25
Gustavo Martin <gusmartin79@gmail.com>
2022-08-03 16:17:31 +0200

    Commit SRC

---
+++ src/datos/Main.java
@@ -0,0 +1,28 @@
+package datos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte myByte = 9;
+        short myShort = 21;
+        int myInt = 30;
+        long myLong = 321654987;
+        float myFloat = 8.5f;
+        double myDouble = 12.25;
+        boolean myBoolean = true;
+        char myChar = 'G';
+        String myString = "Open Bootcamp";
+
+        System.out.println("\n\tEJEMPLO DE TIPOS DE DATOS\n\n\t" +
+                "byte= " + myByte + "\n\t" +
+                "short= "+ myShort +"\n\t" +
+                "int= "+ myInt +"\n\t" +
+                "long= "+ myLong +"\n\t" +
+                "float= "+ myFloat +"\n\t" +
+                "double= "+ myDouble +"\n\t" +
+                "boolean= "+ myBoolean +"\n\t" +
+                "char= "+ myChar +"\n\t" +
+                "String= "+ myString +"\n\t");
+    }
+}
